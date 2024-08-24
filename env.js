a8bee1febf3d6a80ab46d848a07648f2df1e899d
ermao6 <luermaoa@gmail.com>
2024-08-24 08:06:05 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,19 @@
+// API地址
+window.routerBase = 'https://api.008228.xyz/'
+window.settings = {
+  // 站点名称
+  title: '三文鱼vpn机场',
+  // 站点描述
+  description: '绝不收集用户隐私，不留存任何日志记录。让您的上网体验真正做到私密、安全、无痕。',
+  assets_path: '/assets',
+  // 主题色
+  theme: {
+    color: 'default', //可选default、blue、black、、darkblue
+  },
+  // 版本号
+  version: '0.1.1-dev',
+  // 登陆背景
+  background_url: '',
+  // 站点LOGO
+  logo: '',
+}
